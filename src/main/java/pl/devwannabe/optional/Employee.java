cba64428b02c48608c6b2dd60577dc8c779e82a7
Marek <looppega@gmail.com>
2018-09-19 17:54:17 +0200

    Optional

---
+++ src/main/java/pl/devwannabe/optional/Employee.java
@@ -0,0 +1,24 @@
+package pl.devwannabe.optional;
+
+public class Employee {
+
+    private String name;
+    private int age;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/pl/devwannabe/optional/EmployeeRepository.java
@@ -0,0 +1,41 @@
+package pl.devwannabe.optional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class EmployeeRepository {
+
+    private static Map<String, Employee> employees = new HashMap<>(1);
+
+    static {
+        employees.put("Marek", new Employee("Marek",40));
+    }
+
+    private EmployeeRepository() {
+
+    }
+
+    /**
+     * Tradycyjne podejście, metoda może zwrócić null jeżeli imię się nie znajdzie
+     * w mapce.
+     */
+    public static  Employee findEmployee(String name) {
+        return employees.get(name);
+    }
+
+    /**
+     * Podejście z użyciem klasy Optional, metoda zwróci obiekt Employee zawinięty w
+     * Optional lub Optional. <br>
+     * Występują trzy  opcje użycia: <br>
+     * 1. Optional.of(employees.get(name)); <br>
+     * 2. Optional.ofNullable(employees.get(name)); <br>
+     * 3. Optional.empty(); <br>
+     * W pierwszym przypadku jeśli będzie null, to metoda zapakuje go w Optionala i zwróci. <br>
+     * W drugim zwróci pustego Optionala - bezpieczniejsza opcja.
+     * W trzecim zawsze pustego Optionala
+     */
+    public static Optional <Employee> findOptionalEmployee(String name) {
+        return Optional.ofNullable(employees.get(name));
+    }
+}
